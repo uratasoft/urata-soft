b4faea7e0c3c3f3eb37222b5b2ed39b7bad7c61f
CCurata <urata@mtd.biglob.ne.jp>
2024-01-29 15:58:46 +0900

    update

--- ChatGPT×python/ch4/text_embedding.py
+++ ChatGPT×python/ch4/text_embedding.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import tiktoken
-from openai.embeddings_utils import get_embedding
+from openai import get_embedding
 
 embedding_model = "text-embedding-ada-002"
 embedding_encoding = "cl100k_base"
